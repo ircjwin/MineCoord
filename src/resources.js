1e2f3918a6199f1b33e9743cb57e49ee00a01605
ircjwin <christopherjamesirwin@gmail.com>
2024-09-12 20:42:35 -0700

    refactor: combine model commands
    - Create 'job.js' to house Job commands as Subcommands and JobResource commands as a SubcommandGroup
    - Create 'landmark.js' to house Landmark commands as Subcommands
    - Remove Resource commands (Resource table will be prepopulated in future commit)
    - Move .js files into 'src' folder unless they are needed for startup

---
+++ src/resources.js
@@ -0,0 +1,5 @@
+const minecraftData = require('minecraft-data');
+// Blocks, Items, Foods, Mobs, Enchantments, Entities
+
+const mcData = minecraftData('1.20');
+console.log(`Minecraft Version ${mcData.version}: Data Loaded!`);
